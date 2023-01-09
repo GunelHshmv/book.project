733b5845795f226a3b79f7963f30817ea1d78912
GunelHashimova <114391396+GunelHshmv@users.noreply.github.com>
2023-01-09 11:42:07 +0400

    Update booksLib.ts

--- src/app/models/booksLib.ts
+++ src/app/models/booksLib.ts
@@ -5,27 +5,3 @@ export interface bookLibrary{
         pricem:any,
         tittlem:string
 }
-export interface authorData{
-        firstName:string
-        lastName:string
-        books:string
-        birthDate:any
-        birthPlace:string
-        description:string
-}
-export interface loginData{
-        fullName:string
-        password:any
-}
-export interface empList{
-        names:string,
-        surnames:string,
-        positions:string,
-        emails:any,
-        salarys:any,
-        phones:any,
-        ages:any
-}
-export interface comment{
-        comment:string
-}
